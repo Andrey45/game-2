e15d85e040570971830c0bb8dbcc5840a9c3d776
andre <andreykobetz@gmail.com>
2019-10-24 23:15:33 +0400

    add game

--- main.js
+++ main.js
@@ -1,39 +1,77 @@
-import { left } from './move.js';
+import { left, right } from './move.js';
 
 let img = new Image();
-
+let personImg = new  Image();
+let backgroundOffset;
 let canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d");
 
 function Img() {
-    img.src = './bac.jpg';
+    img.src = './background-2.jpg';
 
     img.onload = () => {
-        context.drawImage(img, 0, 0, 5000, 550)
+        context.drawImage(img, 0, 0, img.width, img.height)
     }
 }
-function startgame(){
-    canvas.width = 5000;
-    canvas.height = 600;
+function setBackgroundOffset() {
+    var offset = backgroundOffset + 10;
 
-    context.beginPath();
-    context.moveTo(0, 600);
-    context.lineTo(5000, 600);
-    context.lineWidth = 100;
-    context.closePath();
-    context.strokeStyle = '#10a100';
-    context.stroke()
-    Img()
+    if (offset > 0 && offset < img.width) {
+        backgroundOffset = offset;
+    }
+    else {
+        backgroundOffset = 0;
+    }
+}
+function personInit() {
+
+    personImg.src = person.img;
+
+    personImg.onload = () => {
+        console.log(person.x);
+        //if (person.x >= img.width/2 || person.x === img.width/2){
+            context.translate(-backgroundOffset, 0);
+            context.drawImage(img, 0, 0);
+            context.drawImage(img, img.width, 0);
+            context.translate(backgroundOffset, 0);
+            context.drawImage(personImg, position(),person.y, 150, 150)
+        // } else {
+        //     context.drawImage(img, 0, 0, img.width, img.height)
+        //     context.drawImage(personImg, person.x,person.y, 150, 150)
+        // }
+    }
 }
 
-
+function startgame(){
+    Img();
+    canvas.width = img.width;
+    canvas.height = img.height;
+    personInit();
+}
+function position() {
+    if(person.x < 400){
+        return person.x
+    } else {
+        return 400
+    }
+}
+export function moveLeft(x){
+    person.x = x;
+    context.save()
+    setBackgroundOffset()
+    personInit()
+    context.restore()
+}
 
 let person = {
     x: 20,
-    y: 100,
+    y: 400,
     hp: 100,
-    glass: 0
+    glass: 0,
+    img: './timon.png'
 };
 
-left();
+left(person)
+console.log(`dddd = ${person.x}`)
+right(person);
 startgame();
--- move.js
+++ move.js
@@ -1,8 +1,33 @@
-export function left() {
+import { moveLeft } from './main.js'
+
+let canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d");
+
+export function left(person) {
     document.addEventListener('keydown', function(event) {
         if (event.key === 'ArrowLeft') {
-            let canvas = document.getElementById("canvas")
-
+            canvas = document.getElementById("canvas");
+            person.x +=10;
+            // let left = canvas.style.left;
+            // canvas.style.left = `${person.x}px`;
+            console.log(window.innerWidth/2);
+            moveLeft(person.x)
         }
     });
+}
+export function right(person) {
+    let x;
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowRight') {
+            canvas = document.getElementById("canvas");
+            person.x +=10;
+            moveLeft(person.x)
+        }
+    });
+}
+
+function bacRight(x) {
+    // if (x >= window.innerWidth/2){
+    //     canvas.style.left = `${-x}px`;
+    // }
 }
